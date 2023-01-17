7aa3a572b02f2177b15a9c146d931515f1137121
Melvin Zottola <mzottola@qovery.com>
2023-01-17 10:19:35 +0100

    Commit webhook

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
 
-	e.POST("/webhook", func(c echo.Context) error {
+	e.POST("/webhook-mzo", func(c echo.Context) error {
 		e.Logger.Print("hello webhook updated")
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
