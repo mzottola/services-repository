919a21e0c1fc2772d31c65a988823a5734b64a55
Melvin Zottola <mzottola@qovery.com>
2024-01-25 17:44:50 +0100

    Add tcp server

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -29,9 +32,40 @@ func main() {
 
 	go func() {
 		var e = createEcho()
-		e.Logger.Fatal(e.Start(":" + "4200"))
+		e.Logger.Fatal(e.Start(":" + "3000"))
 	}()
 
-	var e = createEcho()
-	e.Logger.Fatal(e.Start(":" + "3000"))
+	// Listen for incoming connections.
+	l, err := net.Listen("tcp", "localhost:4200")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on localhost:4200")
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+}
+
+// Handles incoming requests.
+func handleRequest(conn net.Conn) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	fmt.Println("Message received")
+	// Close the connection when you're done with it.
+	conn.Close()
 }
