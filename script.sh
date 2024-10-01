42f6b2c61f86077b91956878df9ae94f466c20f5
Melvin Zottola <mzottola@qovery.com>
2024-10-01 16:02:57 +0200

    azert

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@
 
 echo 'Pushing variables...'
 sleep 5
-echo '{ "output_secret_1": { "sensitive": true, "value": "updatedvalue" }, "output_var_2": { "sensitive": false, "value": "myvalue" } }' > /qovery-output/qovery-output.json
+echo '{ "output_secret_1": { "sensitive": true, "value": "updatedvalue" }, "output_var_2": { "sensitive": false, "value": "updatedvar" } }' > /qovery-output/qovery-output.json
 echo 'done'
 
