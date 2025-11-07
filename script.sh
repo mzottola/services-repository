aa180c14f582d4869aeb3423f5fbbfcf7426db1d
Melvin Zottola <mzottola@qovery.com>
2025-11-07 11:13:54 +0100

    Trigger variables update

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 echo 'Pushing variables...'
 
 sleep 1
-echo '{ "output_secret_2": { "sensitive": true, "value": "updatedvalue" }, "output_var_3": { "sensitive": false, "value": "updatedvar" } }' > /qovery-output/qovery-output.json
+echo '{ "output_secret_2": { "sensitive": true, "value": "updatedvalue2" }, "output_var_3": { "sensitive": false, "value": "updatedvar2" } }' > /qovery-output/qovery-output.json
 echo 'done'
 
