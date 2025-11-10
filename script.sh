e132a1b6f42763bb6bca31c91b9517d2912df4dd
Melvin Zottola <mzottola@qovery.com>
2025-11-10 11:47:27 +0100

    boom

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 echo 'Pushing variables...'
 
 sleep 1
-echo '{ "output_secret_2": { "sensitive": true, "value": "updatedvalue4" }, "output_var_3": { "sensitive": false, "value": "updatedvar2" } }' > /qovery-output/qovery-output.json
+echo '{ "output_secret_3": { "sensitive": true, "value": "updatedvalue4" }, "output_var_3": { "sensitive": false, "value": "updatedvar2" } }' > /qovery-output/qovery-output.json
 echo 'done'
 
