1d5e191f85ac611a8867979c852c9f2fee641133
Melvin Zottola <mzottola@qovery.com>
2026-04-07 09:58:02 +0200

    Cronjob

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-echo 'Pushing variables...'
+echo 'Cronjob job in progress'
 
-sleep 1
-echo '{ "output_secret_3": { "sensitive": true, "value": "updatedvalue4" }, "output_var_3": { "sensitive": false, "value": "updatedvar2" } }' > /qovery-output/qovery-output.json
+sleep 2
 echo 'done'
 
