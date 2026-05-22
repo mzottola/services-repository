989d3f8e97394f74b1252b12111fb179ea540b86
Melvin Zottola <mzottola@qovery.com>
2026-05-22 14:05:52 +0200

    feat: wait 60s to terminate

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 echo 'Pushing variables...'
 
-sleep 1
+sleep 60
 echo '{ "output_secret_3": { "sensitive": true, "value": "updatedvalue4" }, "output_var_3": { "sensitive": false, "value": "updatedvar2" } }' > /qovery-output/qovery-output.json
 echo 'done'
 
